0e3b1d768c0306ab2cd8d215abbbd996fae327e3
Chintan <chintan@wohlig.com>
2017-11-29 16:28:58 +0530

    changes

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 var _ = require("lodash");
 var cards = require("./cards");
 
+
+
+
 function rankHands(playerCards) {
     var clonePlayerCards = _.sortBy(_.map(playerCards, function (n) {
         return cards.cardValue(n);
@@ -84,42 +87,4 @@ function rankHands(playerCards) {
     };
 }
 
-
-
-
-var players = [
-
-    {
-        name: "1",
-        cards: ["Ts", "2c", "5s"]
-    },
-    {
-        name: "2",
-        cards: ["Ac", "Kc", "Qc"]
-    },
-    {
-        name: "3",
-        cards: ["2c", "2d", "2h"]
-    },
-    {
-        name: "4",
-        cards: ["5d", "6c", "6s"]
-    },
-    {
-        name: "4",
-        cards: ["Ad", "2d", "4d"]
-    }
-
-
-];
-
-
-_.each(players, function (player) {
-    player.details = rankHands(player.cards);
-    player.score = player.details.score;
-});
-
-var ranking = _.orderBy(players, function (n) {
-    return n.score * -1;
-});
-console.log(ranking);
+module.exports = rankHands;
