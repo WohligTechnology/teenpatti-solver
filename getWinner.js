44951c35bc72b00e353d17e74ec60276baa8d522
Chintan Shah <chintan@wohlig.com>
2019-05-21 11:19:39 +0530

    changes

--- getWinner.js
+++ getWinner.js
@@ -4,12 +4,15 @@ var handJoker1 = teenPattiScore.scoreHandsJokers(
   ["5c", "Kd"]
 );
 
-// var handJoker2 = teenPattiScore.scoreHandsJokers(["Kd", "Qd"], []);
+var handJoker2 = teenPattiScore.scoreHandsJokers(
+  ["Kd", "5h", "6d"],
+  ["5c", "Kd"]
+);
 
-// if (handJoker1.score > handJoker2.score) {
-//   console.log("Player 1 is Winner");
-// } else if (handJoker1.score == handJoker2.score) {
-//   console.log("Split Pot");
-// } else {
-//   console.log("Player 2 is Winner");
-// }
+if (handJoker1.score > handJoker2.score) {
+  console.log("Player 1 is Winner", handJoker1);
+} else if (handJoker1.score == handJoker2.score) {
+  console.log("Split Pot");
+} else {
+  console.log("Player 2 is Winner", handJoker2);
+}
