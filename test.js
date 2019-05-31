92247aff3d3dce808ec4c72e3b3fa83829ffacd3
Chintan Shah <chintan@wohlig.com>
2019-05-31 15:24:57 +0530

    changes

--- test.js
+++ test.js
@@ -19,9 +19,77 @@ function generateCSV(data) {
   }
 }
 
+function getCard(cards) {
+  var cards = _.chain(cards)
+    .split(",")
+    .map(function(n) {
+      return _.capitalize(n);
+    })
+    .value();
+  return cards;
+}
+
+function checkForSingle(data) {
+  try {
+    if (data.Type == "Normal") {
+      var player1Hand = teenPattiScore.scoreHandsNormal(
+        getCard(data["Player 1"])
+      );
+      var player2Hand = teenPattiScore.scoreHandsNormal(
+        getCard(data["Player 2"])
+      );
+    } else if (data.Type == "4 Cards") {
+      var player1Hand = teenPattiScore.scoreHandsFour(
+        getCard(data["Player 1"])
+      );
+      var player2Hand = teenPattiScore.scoreHandsFour(
+        getCard(data["Player 2"])
+      );
+    } else if (data.Type == "2 Cards") {
+      var player1Hand = teenPattiScore.scoreHandsTwo(getCard(data["Player 1"]));
+      var player2Hand = teenPattiScore.scoreHandsTwo(getCard(data["Player 2"]));
+    } else if (data.Type == "Muflis") {
+      var player1Hand = teenPattiScore.scoreHandsLowest(
+        getCard(data["Player 1"])
+      );
+      var player2Hand = teenPattiScore.scoreHandsLowest(
+        getCard(data["Player 2"])
+      );
+    } else if (data.Type == "Joker") {
+      var player1Hand = teenPattiScore.scoreHandsJoker(
+        getCard(data["Player 1"]),
+        _.first(getCard(data["Joker"]))
+      );
+      var player2Hand = teenPattiScore.scoreHandsJoker(
+        getCard(data["Player 2"]),
+        _.first(getCard(data["Joker"]))
+      );
+    }
+  } catch (err) {
+    console.log(err);
+  }
+
+  if (player1Hand && player2Hand) {
+    if (player1Hand.score > player2Hand.score) {
+      data["System Winner"] = "Player1";
+      data["System Winning Condition"] = player1Hand.name;
+      data["System Winning Description"] = player1Hand.desc;
+    } else if (player1Hand.score == player2Hand.score) {
+      data["System Winner"] = "Split Pot";
+      data["System Winning Condition"] = player1Hand.name;
+      data["System Winning Description"] = player1Hand.desc;
+    } else {
+      data["System Winner"] = "Player2";
+      data["System Winning Condition"] = player2Hand.name;
+      data["System Winning Description"] = player2Hand.desc;
+    }
+  }
+}
+
 csv()
   .fromFile(csvFilePath)
   .then(jsonObj => {
-    console.log(jsonObj);
+    // jsonObj = _.slice(jsonObj, 0, 10);
+    _.each(jsonObj, checkForSingle);
     generateCSV(jsonObj);
   });
