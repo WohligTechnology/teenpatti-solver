c42ead4a5b0a8b8cbafead68a86f6d163f70e524
Chintan Shah <chintan@wohlig.com>
2019-06-03 22:27:51 +0530

    changes

--- getWinner.js
+++ getWinner.js
@@ -1,7 +1,7 @@
 var teenPattiScore = require("./index");
-var handJoker1 = teenPattiScore.scoreHandsNormal(["Ac", "Kc", "Qd"]);
+var handJoker1 = teenPattiScore.scoreHandsTwo(["Ac", "Kc"]);
 
-var handJoker2 = teenPattiScore.scoreHandsNormal(["Ad", "2h", "3c"]);
+var handJoker2 = teenPattiScore.scoreHandsTwo(["Ad", "2h"]);
 
 if (handJoker1.score > handJoker2.score) {
   console.log("Player 1 is Winner", handJoker1);
